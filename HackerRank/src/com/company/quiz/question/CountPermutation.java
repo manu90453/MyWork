dea76909bd8a9585bda78a8778ea7be8ce4440e8
manu90453 <manishsharma90453@hotmail.com>
2017-07-28 21:45:08 +0530

    Hacker Rank

---
+++ HackerRank/src/com/company/quiz/question/CountPermutation.java
@@ -0,0 +1,44 @@
+package com.company.quiz.question;
+
+import java.util.Scanner;
+
+public class CountPermutation {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter total test case");
+		int testCase = sc.nextInt();
+		System.out.println("enter value in 2d array");
+		int[] firstNumber = new int[testCase];
+		int[] secondNuber = new int[testCase];
+		
+		for(int i=0; i<testCase; i++) {
+			System.out.println("enter firstNumber: ");
+			firstNumber[i] = sc.nextInt();
+			System.out.println("enter secondNumber: ");
+			secondNuber[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<testCase; i++) {
+			countPerm(firstNumber[i], secondNuber[i]);
+		}
+	}
+	
+	static int countPerm(int first, int second) {
+		int diff = first-second;
+		if(second > first)
+			return 0;
+		
+		if(first>second && diff != second )
+			return 1;
+		
+		if(diff >= second) {
+			
+		}
+		return 1;
+	}
+	
+	static int count()
+
+}
